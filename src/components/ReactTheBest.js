826491b838c77fba5b733464a41241c057ae240c
Daniel Correa <dcorrealenis@gmail.com>
2022-06-14 12:43:50 -0500

    first commit coruse

---
+++ src/components/ReactTheBest.js
@@ -0,0 +1,16 @@
+export default function ReactTheBest(){
+
+
+    return(
+
+      
+
+       <div>
+        
+       <h1>React es lo mejor de la puta historia</h1>
+    
+        </div>
+
+
+    );
+}
